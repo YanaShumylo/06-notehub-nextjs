6a85cab8c60c3994d387ad1be78b9217c7866592
ShumyloYana <zolotushko1@gmail.com>
2025-08-01 16:58:54 +0300

    renaim modal

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -9,7 +9,7 @@ import css from "../../components/NotesPage/NotesPage.module.css";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import Pagination from "../../components/Pagination/Pagination";
 import NoteList from "../../components/NoteList/NoteList";
-import Modal from "../../components/NoteModal/NoteModal";
+import Modal from "../../components/Modal/Modal";
 import NoteForm from "../../components/NoteForm/NoteForm";
 import { fetchNotes } from "../../lib/api";
 import Loader from "../../app/loading";
