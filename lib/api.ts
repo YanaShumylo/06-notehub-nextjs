47cde47405eb9741ab966ba4d7cf4a15797ec10c
ShumyloYana <zolotushko1@gmail.com>
2025-08-01 15:54:25 +0300

    renaim types

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { NewNoteData, Note } from "../type/note";
+import type { NewNoteData, Note } from "../types/note";
 
 const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
